92cc9076f762759458184349798b6d386dca4bea
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2023-08-18 15:04:13 +0200

    Use nlargest aggregation in Q8
    Signed-off-by: Andrey Pavlenko <andrey.a.pavlenko@gmail.com>

--- timedf_benchmarks/h2o/h2o_pandas.py
+++ timedf_benchmarks/h2o/h2o_pandas.py
@@ -45,12 +45,7 @@ def groupby_q7(x):
 
 
 def groupby_q8(x):
-    return (
-        x[~x["v3"].isna()][["id6", "v3"]]
-        .sort_values("v3", ascending=False)
-        .groupby("id6", **gb_params)
-        .head(2)
-    )
+    return x.groupby("id6")["v3"].nlargest(2).reset_index()[["id6", "v3"]]
 
 
 def groupby_q9(x):
